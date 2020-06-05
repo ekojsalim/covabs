ca29e0b9aeb3a60140111b9ac82e27af566b5634
Eko Julianto Salim <ekojsalim@gmail.com>
2020-06-05 22:38:46 +0700

    Update SSL for NLTK download

--- answerer.py
+++ answerer.py
@@ -7,14 +7,6 @@ from nltk.stem.porter import PorterStemmer
 class Answerer:
     def __init__(self):
         import nltk
-        import ssl
-
-        try:
-            _create_unverified_https_context = ssl._create_unverified_context
-        except AttributeError:
-            pass
-        else:
-            ssl._create_default_https_context = _create_unverified_https_context
 
         nltk.download("stopwords")
         nltk.download('punkt')
